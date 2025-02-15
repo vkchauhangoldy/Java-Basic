ded2f8f62e7ae284155f022397f299f683060b00
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2025-02-15 18:58:57 +0530

    We leant to take input in java

---
+++ src/input/Input.java
@@ -0,0 +1,13 @@
+package input;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("Learn How to take inputs in Java");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Your name is: "+ name);
+    }
+}
